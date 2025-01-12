c3ef4441bb153e33c125f6047dbde261e00417b8
Deepak Sharma <dsharma2828@gmail.com>
2025-01-12 10:55:29 +0530

    Restart

---
+++ AuthService/src/test/java/com/deepak/sharma/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepak.sharma.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
