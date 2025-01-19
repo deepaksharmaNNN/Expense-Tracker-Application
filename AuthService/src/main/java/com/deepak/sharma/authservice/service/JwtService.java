5982926e7794fef0ec86ee5f1e8b1d06b2c6383a
Deepak Sharma <dsharma2828@gmail.com>
2025-01-19 09:31:56 +0530

    updated code

--- AuthService/src/main/java/com/deepak/sharma/authservice/service/JwtService.java
+++ AuthService/src/main/java/com/deepak/sharma/authservice/service/JwtService.java
@@ -2,11 +2,15 @@ package com.deepak.sharma.authservice.service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -21,6 +25,27 @@ public class JwtService {
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
+    }
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private String createToken(Map<String, Object> claims, String username) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 5)) // 5 minutes
+                .signWith(getSignKey(), SignatureAlgorithm.ES256)
+                .compact();
+    }
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
     }
     private Claims extractAllClaims(String token) {
         return Jwts
